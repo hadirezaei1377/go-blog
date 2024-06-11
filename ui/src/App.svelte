8388fd2bf44183bac78cb7c7f7a9f86370972fc3
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-06-11 14:20:10 +0330

    Add: frontend auth (Login, GetUserID, Logout)

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,21 +1,37 @@
 <script>
-</script>
-
-<main>
-
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-  }
-</style>
+    // @ts-ignore
+    import GetUserId from "./lib/GetUserID.svelte";
+    // @ts-ignore
+    import Login from "./lib/Login.svelte";
+    // @ts-ignore
+    import Logout from "./lib/Logout.svelte";
+  </script>
+  
+  <main>
+    <div class="box">
+      <GetUserId />
+      <Login />
+      <Logout />
+    </div>
+  </main>
+  
+  <style>
+    :root {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+  
+    main {
+      text-align: center;
+      padding: 1em;
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+    }
+  
+    .box {
+      background-color: #e0e0ee;
+      width: 500px;
+      height: 550px;
+    }
+  </style>
