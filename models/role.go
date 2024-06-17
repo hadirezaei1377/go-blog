63bc68e81f1e7d677f0b548ead7ac029155232b6
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-06-17 14:32:08 +0330

    add: role model and permissions enum

---
+++ models/role.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"go-blog/models/permissions"
+
+	"gorm.io/gorm"
+)
+
+// Any user has a role, and any role has some permissions and accessories in the future (TODO) (logo, cool name and more)
+type Role struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Permissions []permissions.Permission
+}
