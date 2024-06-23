ec61100a901ec72cd36df9dd092917e738fbdfd1
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-06-23 23:10:49 +0330

    update: error handlings

--- controllers/posts.go
+++ controllers/posts.go
@@ -10,37 +10,55 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+package controllers
+
+import (
+	"strconv"
+
+	"github.com/arshamalh/blogo/database"
+	"github.com/arshamalh/blogo/models"
+	"github.com/arshamalh/blogo/tools"
+	"github.com/gin-gonic/gin"
+)
+
 type PostRequest struct {
 	Title      string   `form:"title" json:"title" binding:"required"`
 	Content    string   `form:"content" json:"content" binding:"required"`
 	Categories []string `form:"categories" json:"categories"`
 }
 
-func CreatePost(c *gin.Context) {
+func CreatePost(ctx *gin.Context) {
 	var post PostRequest
-	user_id, _ := tools.ExtractUserID(c)
-	if c.BindJSON(&post) == nil {
-		catgs := []models.Category{}
-		for _, catg := range post.Categories {
-			catgs = append(catgs, models.Category{Name: catg})
-		}
-
-		new_post := models.Post{
-			Title:      post.Title,
-			Content:    post.Content,
-			AuthorID:   user_id,
-			Categories: catgs,
-		}
-		post_id, _ := database.CreatePost(&new_post)
-		c.JSON(200, gin.H{
-			"message": "Post created successfully",
-			"post_id": post_id,
-		})
+	user_id, err := tools.ExtractUserID(ctx)
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
 	}
+	if err := ctx.BindJSON(&post); err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	catgs := []models.Category{}
+	for _, catg := range post.Categories {
+		catgs = append(catgs, models.Category{Name: catg})
+	}
+
+	new_post := models.Post{
+		Title:      post.Title,
+		Content:    post.Content,
+		AuthorID:   user_id,
+		Categories: catgs,
+	}
+	post_id, _ := database.CreatePost(&new_post)
+	ctx.JSON(200, gin.H{
+		"message": "Post created successfully",
+		"post_id": post_id,
+	})
 }
 
 func GetPost(c *gin.Context) {
-	post_id, _ := strconv.Atoi(c.Param("id"))
+	post_id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
 	post, _ := database.GetPost(uint(post_id))
 	c.JSON(200, gin.H{
 		"post": post,
